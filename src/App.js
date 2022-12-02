35efa39580def9885693f4ddb04d246c816e860a
Heriniavo <sampilahy.heriniavo.com>
2022-12-03 00:39:28 +0300

    Translation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import TextesJuridiques from './components/TextesJuridiques';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Thematique from "./components/Thematique";
 import RechercheAvance from "./components/RechercheAvance";
-import { getTextesJuridiques } from './classes/Util.js';
+import {getTextesJuridiques} from './classes/Util.js';
+import {useTranslation} from "react-i18next";
 
 const textes = getTextesJuridiques();
 
 function App() {
+    const {t, i18n} = useTranslation();
 
-  console.log(textes);
+    //Creating a method to change the language onChnage from select box
+    const changeLanguageHandler = (e) => {
+        const languageValue = e.target.value
+        i18n.changeLanguage(languageValue);
+    }
 
-  return (
-    <BrowserRouter>
-      <Navbar /><br /><br /><br /><br />
-      <Routes>
-        <Route path='/' element={<Navbar />} />
-        <Route path='thematiques' element={<Thematique />} />
-        <Route path='rechercheAvance' element={<RechercheAvance />} />
-        <Route path='textesJuridiques' element={<TextesJuridiques textes={textes} />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar/><br/><br/><br/><br/>
+            <Routes>
+                <Route path='/' element={<Navbar/>}/>
+                <Route path='thematiques' element={<div>
+
+                    <select className="custom-select" style={{width: 200}} onChange={changeLanguageHandler}>
+                        <option value="fr">Français</option>
+                        <option value="mg">Malagasy</option>
+                    </select>
+
+
+                    <Thematique/></div>}/>
+                <Route path='rechercheAvance' element={<RechercheAvance/>}/>
+                <Route path='textesJuridiques' element={<TextesJuridiques textes={textes}/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
