a462399bd62d114e09362d68dfd81ac9bb761857
AndoMickael <ando.mickael25@gmail.com>
2022-12-02 20:30:25 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { getTextesJuridiques } from './classes/Util.js';
 const textes = getTextesJuridiques();
 
 function App() {
-  
+
   console.log(textes);
 
   return (
@@ -17,14 +17,8 @@ function App() {
       <Navbar /><br /><br /><br /><br />
       <Routes>
         <Route path='/' element={<Navbar />} />
-        <Route
-          path='thematiques'
-          element={
-            <div>
-              <Thematique />
-            </div>
-          }
-        />
+        <Route path='thematiques' element={<Thematique />} />
+        <Route path='rechercheAvance' element={<RechercheAvance />} />
         <Route path='textesJuridiques' element={<TextesJuridiques textes={textes} />} />
       </Routes>
     </BrowserRouter>
